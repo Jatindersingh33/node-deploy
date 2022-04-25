6fa52e8cfe9ff2283b5556bc54781ca9bb870f75
singhjat <jatinder.singh@tabcorp.com.au>
2022-04-25 15:23:29 +1000

    update port

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 1744;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
+  res.send('<h1>ReverB API</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
 })
 
 app.get('/products', (req, res) => {
@@ -20,6 +20,6 @@ app.get('/products', (req, res) => {
 })
 
 app.listen(port, ()=> {
-  console.log(`Demo app is up and listening to port: ${port}`);
+  console.log(`ReverB api is up and listening to port: ${port}`);
 })
  
